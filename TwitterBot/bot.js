87ee99d8f89e704659e69ec9c3ae9a8b91d6c714
Justin Pacquing <pacqqu@gmail.com>
2018-06-18 14:08:28 -0400

    Utilizing Twitter and Genius APIs

--- TwitterBot/bot.js
+++ TwitterBot/bot.js
@@ -1,2 +1,6 @@
+var Twit = require('twit');
 var config = require('./config');
 var configgenius = require('./config-genius');
+  var T = new Twit(config)
+  var Genius = require("genius-api");
+  var genius= new Genius(configgenius);
