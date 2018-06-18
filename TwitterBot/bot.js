635b96981e996ef87b7cf3ec96af8d11de1be49c
Justin Pacquing <pacqqu@gmail.com>
2018-06-18 14:09:33 -0400

    Using SetInterval to ensure not going over API call limit

--- TwitterBot/bot.js
+++ TwitterBot/bot.js
@@ -1,6 +1,19 @@
 var Twit = require('twit');
 var config = require('./config');
 var configgenius = require('./config-genius');
+setInterval(function(){
   var T = new Twit(config)
   var Genius = require("genius-api");
   var genius= new Genius(configgenius);
+  T.get('statuses/mentions_timeline', {screen_name: 'spiritlyricbot' }, function (err,data,response) {
+    while (err !== undefined){
+      setTimeout(function(){
+        T.get('statuses/mentions_timeline', {screen_name: 'spiritlyricbot' }, function (errWait,dataWait,responseWait) {
+          data = dataWait;
+          err = errWait;
+          response = responseWait;
+        });
+      },
+      100000);
+    }
+},5000);
